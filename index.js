16f394ae03dd364601e351fdb4b19c2931b9b45b
IronWalrus68 <camsjobhunting@gmail.com>
2024-01-08 21:25:27 +0000

    create slot machine ui

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const ExpressError = require('./utils/ExpressError')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,9 +25,9 @@ app.all('*', (req, res, next) => {
 })
 //error handler
 app.use((err, req, res, next) => {
-    const {statusCode = 500 } = err;
-    if(!err.message) err.message = 'Something Went Wrong :(';
-    res.status(statusCode).render('error', {err});
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong :(';
+    res.status(statusCode).render('error', { err });
 })
 
 const PORT = process.env.PORT || 3000;
